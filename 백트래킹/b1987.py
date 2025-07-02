3db53a9156fc4dc1c42710d829311e0b88404c37
imchanchan <chaniya918@gmail.com>
2025-07-02 10:18:03 +0900

    시간초과해결1 : visit없애기. (안쓰임.)

--- 백트래킹/b1987.py
+++ 백트래킹/b1987.py
@@ -9,7 +9,7 @@ R,C = map(int, input().split(' '))
 alp_graph = [list(input()) for _ in range(R)]
 
 
-def dfs(q,visit,check_alp):
+def dfs(q,check_alp):
     while q:
         x, y, cnt = q.popleft()
 
@@ -17,29 +17,25 @@ def dfs(q,visit,check_alp):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx<0 or nx>=len(alp_graph) or ny<0 or ny>=len(alp_graph[0]) or (nx, ny) in visit:
+            if nx<0 or nx>=len(alp_graph) or ny<0 or ny>=len(alp_graph[0]):
                 continue
             if alp_graph[nx][ny] in check_alp:
                 continue
 
             q.append([nx, ny, cnt+1])
-            visit.append((nx,ny))
             check_alp.append(alp_graph[nx][ny])
-            # print("q", q)
-            # print("visit", visit)
-            # print("check_alp", check_alp)
+            print("q", q)
+            print("check_alp", check_alp)
 
-            dfs(q, visit, check_alp)
+            dfs(q, check_alp)
 
             res.append(len(check_alp))
             check_alp.pop()
-            visit.pop()
 
 # solve
 q = deque()
 q.append([0,0,0]) # x, y, cnt
-visit = list()
-visit.append((0,0))
+
 check_alp = list()
 check_alp.append(alp_graph[0][0])
 
@@ -47,6 +43,6 @@ dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 
 res = []
-dfs(q, visit, check_alp)
+dfs(q, check_alp)
 
 print(max(res))
