6fc27ddd855ef1c0393f768dd50b14f790962013
imchanchan <chaniya918@gmail.com>
2025-07-03 20:49:08 +0900

    시간초과 해결3 : 딕셔너리로 중복확인

--- 백트래킹/b1987.py
+++ 백트래킹/b1987.py
@@ -1,19 +1,13 @@
 '''
-[문제]: 알파벳/ dfs 백트래킹
+[문제]: 알파벳/ dfs 백트래킹 + 비트마스크
 '''
 
 from collections import deque
 
-# input
-R,C = map(int, input().split(' '))
-alp_graph = [list(input()) for _ in range(R)]
-
-max_length = 0
-
-def dfs(x, y, check_alp):
+def dfs(x, y, visited):
 
     global max_length 
-    max_length = max(max_length, len(check_alp))
+    max_length = max(max_length,  sum(1 for v in visited.values() if v == 1))
 
     for i in range(4):
         nx = x + dx[i]
@@ -21,24 +15,28 @@ def dfs(x, y, check_alp):
 
         if nx<0 or nx>=len(alp_graph) or ny<0 or ny>=len(alp_graph[0]):
             continue
-        if alp_graph[nx][ny] in check_alp:
+        if visited[alp_graph[nx][ny]]==1 :
             continue
 
-        check_alp.add(alp_graph[nx][ny])
-        # print("check_alp", check_alp)
+        visited[alp_graph[nx][ny]] = 1
+        
+        dfs(nx, ny, visited)
 
-        dfs(nx, ny, check_alp)
+        visited[alp_graph[nx][ny]] = 0
 
-        check_alp.remove(alp_graph[nx][ny])
+# input
+R,C = map(int, input().split(' '))
+alp_graph = [list(input()) for _ in range(R)]
+
 
 # solve
-
-check_alp = set()
-check_alp.add(alp_graph[0][0])
+max_length = 0
+visited = {i: 0 for i in ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "N", "M", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]}
+visited[alp_graph[0][0]] = 1
 
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 
-dfs( 0, 0, check_alp)
+dfs( 0, 0, visited)
 
 print(max_length)
