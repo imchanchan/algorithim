f5096c1c76165df0abc537105340eaa4ebf2a6b6
imchanchan <chaniya918@gmail.com>
2025-09-04 16:32:02 +0900

    경우의 수 backtracking 함수수

--- 백트래킹/경우의수.py
+++ 백트래킹/경우의수.py
@@ -6,7 +6,11 @@ itertool없이 날것으로 구현
 # [순열 (Permutation)]
 # arr에서 r개를 "중복없이 순서고려" 뽑는 경우의수
 # 단, arr = [1,2,3,4] 같은 수가 없는 배열
-def perm(arr, r, used=[], path=[]):
+def perm(arr, r, used=None, path=None):
+    if used is None:
+        used = []
+    if path is None:
+        path = []
     if len(path) == r:
         print(path)
         return
@@ -27,7 +31,9 @@ print('- - - - -')
 # [조합 (Combination)]
 # arr에서 r개를 "중복없이 순서no고려" 뽑는 경우의수
 # 단, arr = [1,2,3,4] 같은 수가 없는 배열
-def comb(arr, r, idx=0, path=[]):
+def comb(arr, r, idx=0, path=None):
+    if path is None:
+        path = []
     if len(path) == r:
         print(path)
         return
@@ -36,7 +42,6 @@ def comb(arr, r, idx=0, path=[]):
         comb(arr, r, i+1, path)
         path.pop()
 
-comb([1, 2, 3], 2)
 
 print('- - - - -')
 
@@ -44,7 +49,9 @@ print('- - - - -')
 # [중복순열 (Permutation)]
 # arr에서 r개를 "중복해서 순서고려" 뽑는 경우의수
 # 단, arr = [1,2,3,4] 같은 수가 없는 배열
-def repeat_perm(arr, r, path=[]):
+def repeat_perm(arr, r, path=None):
+    if path is None:
+        path = []
     if len(path) == r:
         print(path)
         return
@@ -61,7 +68,9 @@ print('- - - - -')
 # [순열 (Permutation)]
 # arr에서 r개를 "중복없이 순서고려" 뽑는 경우의수
 # 단, arr = [1,2,3,4] 같은 수가 없는 배열
-def repeat_comb(arr, r, idx=0, path=[]):
+def repeat_comb(arr, r, idx=0, path=None):
+    if path is None:
+        path = []
     if len(path) == r:
         print(path)
         return
